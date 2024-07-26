1cbac858253a37fc7b941d210ec09a5f686cb065
Isreli Reichman <srul36@gmail.com>
2024-07-25 22:36:54 -0600

    Added help strings to args. Created parse_email and check_file functions. Working on convert_to_pdf function.

--- argparse_class.py
+++ argparse_class.py
@@ -3,10 +3,18 @@ Handling command line args.
 """
 import argparse
 
-usage = ""
-parser = argparse.ArgumentParser(prog="Email2PDF", usage="", add_help=True)
-parser.add_argument("-s", "--source", type=str, nargs="?", help="")
-parser.add_argument("-o", "--output", type=str, nargs=1, help="")
-parser.add_argument("-on", "--output_name", type=str, nargs=1, help="")
+def get_args():
+    """
+    Parse command line arguments.
+    :return: Return parsed args.
+    """
+    usage = ""
+    parser = argparse.ArgumentParser(prog="Email2PDF", usage="", add_help=True)
+    parser.add_argument("-s", "--source", type=str, nargs="?", help="Coma seperated list of eml files.")
+    parser.add_argument("-o", "--output", type=str, nargs=1, help="An output path, if different from the "
+                                                                  "working path.")
+    parser.add_argument("-on", "--output_name", type=str, nargs=1, help="Pattern to use for file name. "
+                                                                        "The output file will be added a number if more"
+                                                                        "then one file is processed.")
 
-args = parser.parse_args()
+    return parser.parse_args()
--- main.py
+++ main.py
@@ -1,31 +1,61 @@
 import mailparser
 from weasyprint import HTML as html2pdf
-from argparse_class import parser
 from loguru import logger
+import os
+from argparse_class import get_args
 
-def parse_mail(mail: str):
+
+def parse_mail(mail: str) -> dict:
+    """
+    Parse emails for data.
+    :param mail: location of an email file as a string.
+    :return: a dictionary with information from the email. Including the body of the email.
+    """
     # Parse eml
-    mail = mailparser.parse_from_file(mail)
+    logger.info(f"Parsing email from string {mail}")
+    try:
+        mail = mailparser.parse_from_file(mail)
+    except Exception as error:
+        logger.error(f"Failed to parse email with error\n{error}")
 
-    # Find info in eml
+    # Find info in email object.
     subject = mail.subject
     date = mail.date
     body = mail.body
     body_html_only = body.split("<head>")[1]
     body_html_only_fixed = "<head>" + body_html_only
-    return {"subject": subject, "date": date, "body": body_html_only_fixed}
+    email_data = {"subject": subject, "date": date, "body": body_html_only_fixed}
+    logger.info(f"email was parsed successfully. Email info:\n{email_data}")
+    return email_data
 
 
+def check_file(file_with_path: str) -> dict:
+    """
+    Checks if a file exists and is of the right format.
+    :param file: the location of a file in the OS.
+    :return: Boolean.
+    """
+    # Check that file exists.
+    if not os.path.exists(file_with_path):
+        logger.error(f"Could not find {file_with_path}")
+        return {"file_exists": False, "message": "File does not exist."}
+    # Find the file's extension.
+    file_extension = file_with_path.split(".")[1]
+    # Add file validation?
+    return {"file_exists": True, "message": f"{file_extension}"}
 
 
-
+def convert_to_pdf(html_string: str, output output_naming_template: str) ->
 if __name__ == "__main__":
     print("Starting program...")
 
-mail = parse_mail("email.eml")
+args = get_args()
+
+if args.source:
+     check_file()
+# mail = parse_mail("email.eml")
 
 print(f"{mail["subject"]}\n{mail["date"]}\n{mail["body"]}")
 
 html2pdf(string=mail["body"]).write_pdf("pdf.pdf")
-"restructure"
 
