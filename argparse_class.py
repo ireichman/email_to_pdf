64a5e34cb353a57c14551c9d163caf8b8eb955c6
Isreli Reichman <srul36@gmail.com>
2024-07-29 21:59:07 -0600

    Added parsing of emails and handling of argparse args. Edited argparse function.

--- argparse_class.py
+++ argparse_class.py
@@ -10,11 +10,13 @@ def get_args():
     """
     usage = ""
     parser = argparse.ArgumentParser(prog="Email2PDF", usage="", add_help=True)
-    parser.add_argument("-s", "--source", nargs="+", help="Coma separated list of eml files.")
-    parser.add_argument("-o", "--output", type=str, nargs=1, help="An output path, if different from the "
-                                                                  "working path.")
+    parser.add_argument("-s", "--source", required=True, nargs="+", help="Coma separated list of eml files.")
+    parser.add_argument("-o", "--output", required=True, type=str, nargs=1, help="list of output paths, "
+                                                                                   "if different from the working path. ")
+
     parser.add_argument("-on", "--output_name", type=str, nargs=1, help="Pattern to use for file name. "
                                                                         "The output file will be added a number if more"
-                                                                        "then one file is processed.")
+                                                                        "then one file is processed. If no name is "
+                                                                        "given, the email's file name will be used.")
 
     return parser.parse_args()
--- main.py
+++ main.py
@@ -29,33 +29,73 @@ def parse_mail(mail: str) -> dict:
     return email_data
 
 
-def check_file(file_with_path: str) -> dict:
+def check_file_or_path_exists(file_or_path: str) -> bool:
     """
     Checks if a file exists and is of the right format.
-    :param file: the location of a file in the OS.
+    :param file_or_path: A string which is a path of a file with path.
     :return: Boolean.
     """
     # Check that file exists.
-    if not os.path.exists(file_with_path):
-        logger.error(f"Could not find {file_with_path}")
-        return {"file_exists": False, "message": "File does not exist."}
-    # Find the file's extension.
-    file_extension = file_with_path.split(".")[1]
-    # Add file validation?
-    return {"file_exists": True, "message": f"{file_extension}"}
+    if not os.path.exists(file_or_path):
+        logger.error(f"Could not find {file_or_path}")
+        return False
+    else:
+        return True
+
+# def check_file_extension(file_with_path: str) -> bool:
+#     # Find the file's extension.
+#     try:
+#         file_extension = file_with_path.split(".")[1]
+#     except Exception as error:
+#         logger.error(f"Could not find file's extension. Error:\n{error}")
+#
+#     if
+#     # Add file validation?
+#     return {"file_exists": True, "message": f"{file_extension}"}
 
 
-def convert_to_pdf(html_string: str, output output_naming_template: str) ->
+def convert_to_pdf(html_string: str, output, output_naming_template: str) -> bool:
+
+    pass
 if __name__ == "__main__":
     print("Starting program...")
 
 args = get_args()
 
+# Compile a list of files to convert and check if they exist in the OS.
 if args.source:
-     check_file()
-# mail = parse_mail("email.eml")
+    logger.info(f"Argument received from source parameter: {args.source}")
+    list_of_emails: list = args.source
+    logger.info(f"Files to convert to PDF:\n{list_of_emails}")
+    # Checking that the email files exist in the OS.
+    list_of_emails_validated = filter(check_file_or_path_exists, list_of_emails)
+    # Comparing the list of emails from argparse and the subsection of the same files that exist in the OS.
+    files_not_in_the_os = list(set(list_of_emails) ^ set(list_of_emails_validated))
+    logger.info(f"Files that are not found in the OS: {files_not_in_the_os}")
+    print(f"The following files were not found on your system:\n{files_not_in_the_os}")
+    if not list_of_emails_validated:
+        print("Files were not found on the system.\nExiting...")
+        exit()
 
-print(f"{mail["subject"]}\n{mail["date"]}\n{mail["body"]}")
+# Check that args.output gives a real path.
+if args.output:
+    logger.info(f"Argument received from output parameter: {args.output}")
+    output_path: str = args.output
+    # Check if path exist in the system.
+    path_validated = check_file_or_path_exists(output_path)
+    if not path_validated:
+        print(f"Path does not exist: {output_path}")
+        exit()
 
-html2pdf(string=mail["body"]).write_pdf("pdf.pdf")
+if args.output_name:
+    logger.info(f"Argument received from output_name parameter: {args.output_name}")
+    output_file_name: str = args.output_name
 
+# Parsing the emails and makes a list of strings. Each string is an HTML.
+list_of_emails_parsed = map(parse_mail, list_of_emails_validated)
+
+# print(f"{mail["subject"]}\n{mail["date"]}\n{mail["body"]}")
+
+# html2pdf(string=mail["body"]).write_pdf("pdf.pdf")
+
+import pdb; pdb.set_trace()
