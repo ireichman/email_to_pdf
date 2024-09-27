d530d9576a21c6c2a4aea0e5bfcaf4630c945676
Isreli Reichman <srul36@gmail.com>
2024-09-27 00:23:08 -0600

    Created HtmlToPdf class in html_to_pdf.py. Need to import to main and test.

---
+++ html_to_pdf.py
@@ -0,0 +1,26 @@
+"""
+Class for converting HTML to PDF.
+"""
+import pdfkit
+from loguru import logger
+
+
+class HtmlToPdf:
+
+    def __init__(self, html_data: str, output_file_name_and_path: str, input_format: str = "file",
+                 verbosity: bool = None, css: list = None):
+
+        self.html_data: str = html_data
+        self.output: str = output_file_name_and_path
+        self.input_format: str = input_format
+        self.verbosity: bool = verbosity
+        self.css: list = css
+
+    def create_pdf(self):
+
+        try:
+            pdfkit.from_file(input=self.html_data, output_path=self.output)
+        except Exception as error:
+            logger.error(f"Error while converting HTML to PDF. \n{error}")
+            return False
+        return True
