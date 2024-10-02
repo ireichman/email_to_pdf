417b9de00b7343f1e78bd69d0f8c8eb0defbc359
Isreli Reichman <srul36@gmail.com>
2024-10-01 19:27:10 -0600

    Added usage and description to argparse. Added verbose to argparse.

--- argparse_funct.py
+++ argparse_funct.py
@@ -5,6 +5,11 @@ import argparse
 
 usage = """To run this program, wkhtmltopdf needs to be installed on the system. 
             Run this program with python main.py -s <path-to_email> -o <path/and/name/for_output_file.pdf> """
+
+description = ("Converting email files to PDFs. There are a few convenient file naming schemes available. Depending on "
+               "the specified options, the program will either keep the email's file name or use a given name. If more "
+               "then one file with that name exists, the program will add a serial number at the end (e.g.: file-1, "
+               "file-2, etc.). It is also possible to save files to a different directory.")
 def get_args():
     """
     Parse command line arguments.
@@ -13,7 +18,7 @@ def get_args():
     usage = ("Run tha application using `python main.py -source <file1.eml file2.eml>` This will convert the emails while"
              " keeping their file names.")
 
-    parser = argparse.ArgumentParser(prog="Email2PDF", usage="", add_help=True, description="")
+    parser = argparse.ArgumentParser(prog="Email2PDF", usage=usage, add_help=True, description=description)
     parser.add_argument("-s", "--source", required=True, nargs="+", help="Coma separated list of eml files.")
     parser.add_argument("-o", "--output", required=False, type=str, nargs=1, help="Specify an output path, "
                                                                                    "if different from the working path.")
@@ -22,5 +27,6 @@ def get_args():
                                                                         "The output file will be added a number if more"
                                                                         " then one file is processed. If no name is "
                                                                         "given, the email's file name will be used.")
+    parser.add_argument("-v", "--verbose", type=bool, help="")
 
     return parser.parse_args()
