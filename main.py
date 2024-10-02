e86151066dee4a3388a9432e1221186037750a37
Isreli Reichman <srul36@gmail.com>
2024-10-01 20:38:31 -0600

    Cleaned console messages and add a few more for clarity. Changed PDFKit verbosity to match argparse verbose value.

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from utils import pdf_naming, parse_email, check_file_or_path_exists
 # TODO: Add option for overwriting files???
 # TODO: Add option to print multiple emails to 1 pdf.
 # TODO: Add ability to get date from email (both metadata and text) for use with file name.
-# TODO: Add verbosity to argparse. Hide all logging messages unless logging is True.
+# TODO: 1
 
 if __name__ == "__main__":
     args = get_args()
-    print("Starting program...")
+    print("-" * 50)
+    print("Starting Email to PDF...")
+    print("-" * 50)
+
 logger.remove()
 if args.verbose:
     print("Verbose messaging enabled.")
@@ -25,10 +28,12 @@ if args.source:
     logger.info(f"Files to convert to PDF:\n{list_of_emails}")
     # Checking that the email files exist in the OS.
     list_of_emails_validated = list(filter(check_file_or_path_exists, list_of_emails))
+    print(f"Found the following email files:\n{list_of_emails_validated}")
     # Comparing the list of emails from argparse and the subsection of the same files that exist in the OS.
     files_not_in_the_os = list(set(list_of_emails) ^ set(list_of_emails_validated))
     logger.info(f"Files that are not found in the OS: {files_not_in_the_os}")
-    print(f"The following files were not found on your system:\n{files_not_in_the_os}")
+    if files_not_in_the_os:
+        print(f"The following files were not found on your system:\n{files_not_in_the_os}")
     if not list_of_emails_validated:
         print("Files were not found on the system.\nExiting...")
         exit()
@@ -58,8 +63,9 @@ logger.info(f"Parsed the list of email.")
 
 # Convert HTML string from parsed email to PDF.
 for email in list_of_emails_parsed:
+    print(f"Processing {email["email_file"]}")
     pdf_name = pdf_naming(naming_pattern=output_file_name, output_path=path_validated, email_name=email["email_file"])
-    pdf_o = PDFKit(url_or_file=email["body"], type_="string", verbose=True)
+    pdf_o = PDFKit(url_or_file=email["body"], type_="string", verbose=args.verbose)
     pdf_o.to_pdf(path=pdf_name)
     logger.success(f"Converted {email["email_file"]} to {pdf_name} successfully.")
     print(f"Converted {email["email_file"]} to {pdf_name} successfully.")
