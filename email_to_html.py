7ec5f863fdea66d9cc462058744a2a8f33bf5f0e
Isreli Reichman <srul36@gmail.com>
2024-10-24 01:09:12 -0600

    Added tmp folder for temp files. Need to add mechanism for deleting temp files. Added to is_msg_multipart method, it now also checks for subtype. Multipart parser method does not return html.

--- email_to_html.py
+++ email_to_html.py
@@ -1,6 +1,8 @@
 """
 Class for converting emails to HTML
 """
+import sys
+from base64 import decode
 from email.parser import BytesParser
 from email import policy
 from loguru import logger
@@ -31,8 +33,17 @@ class EmailToHtml:
             self.msg = None
 
     def is_msg_multipart(self):
+
+        logger.info(f"Checking if email is multipart with files.")
         if self.msg:
-            self.msg_is_multipart: bool = self.msg.is_multipart()
+            if self.msg.is_multipart():
+                if self.msg.get_content_subtype() == "related":
+                    logger.info(f"Email is multipart/ related.")
+                    return True
+                else:
+                    return False
+            else:
+                return False
 
     def msg_object_to_html(self):
 
@@ -46,12 +57,10 @@ class EmailToHtml:
         self.email_subject: str = email_msg["subject"]
         self.email_date: str = email_msg["date"]
 
-        if self.msg_is_multipart:
-
-
-
-            pass
-
+        # Checking if email is multipart.
+        if self.is_msg_multipart():
+            logger.info(f"{self.email_file} is multipart/ related")
+            email_body_content = self.multipart_with_files_parser()
         else:
         # Get the body of the email message.
             try:
@@ -64,9 +73,45 @@ class EmailToHtml:
                 email_body_content = email_body.get_content()
             except Exception as error:
                 logger.error(f"Error while getting email body's content. Error:\n{error}")
-
+        email_body_content = email_body_content or ""
         return email_body_content
 
+    def multipart_with_files_parser(self):
+
+        # variables for html and email files.
+        html_part: str = ""
+        related_parts: set = {}
+
+        # Getting all the parts of the multipart email.
+        try:
+            email_parts: object = self.msg.iter_parts()
+            logger.debug(f"Parts in {self.email_file}:\n{email_parts}")
+        except Exception as error:
+            logger.error(f"Error getting parts from {self.email_file}. Error:\n{error}")
+            sys.exit(1)
+
+        for part in email_parts:
+            logger.debug(f"Processing {self.email_file} part {part}")
+            content_type = part.get_content_type()
+            if content_type == "text/html":
+                html_part = part.get_payload(decode=True).decode(part.get_content_charset())
+            elif content_type.startswith('image/'):
+                # Extract images and add them to related_parts var
+                cid = part["Content-ID"]
+                if cid:
+                    cid = cid.strip("<>")
+                    related_parts[cid] = part
+
+        # Replace cid reference with actual inline image.
+        for cid, part in related_parts.items():
+            # Decode the email part.
+            part_data = part.get_payload(decode=True)
+            # save the email part
+            part_file_name = part.get_filename() or f"{cid}.png"
+            with open(f"tmp/{part_file_name}", 'wb') as part_file:
+                part_file.write(part_data)
+            html_content = html_part.replace(f"cid:{cid}", f"tmp/{part_file_name}")
+        return html_content
 
 
 
